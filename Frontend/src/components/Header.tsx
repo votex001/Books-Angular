d9e078f0097ee677d3ce4e5d632ec7c0119b34b1
votex001 <jeneea45@gmail.com>
2024-11-25 21:46:01 +0200

    more readable format

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -5,21 +5,44 @@ import logo from "/imgs/logo.svg";
 import { connect } from "react-redux";
 import { RootState } from "../../store/store";
 import { User } from "../assets/models/user.model";
+import { httpService } from "../services/http.service";
 
 interface HeaderProps {
   user: User | null;
 }
 
 export class _Header extends Component<HeaderProps> {
-  getLinks = () => {
+  get getLinks() {
     const { user } = this.props;
     return [
       { title: "Home", to: "/" },
       { title: "Profile", to: "/profile", hidden: !user },
       { title: "Login", to: "/login", hidden: user },
-      { title: "Sign out", to: "/logout", hidden: !user },
-    ];
-  };
+      {
+        title: "Sign out",
+        to: "",
+        hidden: !user,
+        onClick: async (e: React.MouseEvent<HTMLAnchorElement>) => {
+          e.preventDefault();
+          try {
+            await httpService.post("auth/logout");
+            window.location.reload();
+          } catch (err) {
+            console.log(err);
+          }
+        },
+      },
+    ].map(
+      (link) =>
+        !link.hidden && (
+          <li key={link.to}>
+            <Link to={link.to} onClick={link.onClick || undefined}>
+              {link.title}
+            </Link>
+          </li>
+        )
+    );
+  }
   render(): ReactNode {
     return (
       <section className="header">
@@ -29,16 +52,7 @@ export class _Header extends Component<HeaderProps> {
             <h1>MyBook</h1>
           </Link>
           <nav className="navigation">
-            <ul className="list">
-              {this.getLinks().map(
-                (link) =>
-                  !link.hidden && (
-                    <li key={link.to}>
-                      <Link to={link.to}>{link.title}</Link>
-                    </li>
-                  )
-              )}
-            </ul>
+            <ul className="list">{this.getLinks}</ul>
           </nav>
         </main>
       </section>
