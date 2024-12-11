ff0af9c03be9d000ffba10db3ce9c24869f930d5
votex001 <jeneea45@gmail.com>
2024-12-11 23:49:05 +0200

    gets only id and not all book

--- Backend/apis/userFavorites/favorites.controller.js
+++ Backend/apis/userFavorites/favorites.controller.js
@@ -1,11 +1,12 @@
 import { loggerService } from "../../services/logger.service.js";
+import { booksService } from "../books/books.service.js";
 import { userFavService } from "./favorites.service.js";
 
 export async function myFav(req, res) {
   try {
     const userId = req.loggedinUser.id;
     const userBooks = await userFavService.getUserBooks(userId);
-    if (!userBooks) return res.status(404).send({ error: "Books not found" });
+    // if (!userBooks) return res.status(404).send({ error: "Books not found" });
     res.send(userBooks);
   } catch (e) {
     console.log(e);
@@ -17,15 +18,15 @@ export async function myFav(req, res) {
 export async function postMyFovBook(req, res) {
   try {
     const userId = req.loggedinUser.id;
-    const { book } = req.body;
+    const { bookId } = req.body;
+    const book = await booksService.getById(bookId);
     const ans = await userFavService.save(userId, book);
+    console.log(userId);
     res.send(ans);
   } catch (e) {
     console.log(e);
     loggerService.error(e);
-    return res
-      .status(500)
-      .send({ error: "Couldn't save books to library of user" });
+    res.status(500).send({ error: "Couldn't save books to library of user" });
   }
 }
 
