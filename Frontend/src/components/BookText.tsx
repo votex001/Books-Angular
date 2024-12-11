e9e3b7f4f1376ba28801ea3b502adf6a8d82ddb7
votex001 <jeneea45@gmail.com>
2024-12-11 21:53:40 +0200

    reformate code

--- Frontend/src/components/BookText.tsx
+++ Frontend/src/components/BookText.tsx
@@ -1,5 +1,6 @@
-import  { Component } from "react";
+import { Component } from "react";
 import { RouteComponentProps, withRouter } from "react-router-dom";
+import { bookService } from "../services/bookService";
 
 interface Params {
   id: string;
@@ -19,50 +20,22 @@ class _bookText extends Component<BookTextProps, BookTextState> {
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
     const { params } = this.props.match;
-    await this.fetchText(params.id);
+    this.getText(params.id);
   }
 
-  fetchText = async (id: string) => {
+  getText = async (id: string) => {
     try {
-      const response = await fetch(`http://127.0.0.1:2027/api/books/${id}/txt`);
-      if (response.ok) {
-        const textData = await response.text();
-        const processedTxt = this.processHtml(textData);
-        this.setState({ text: processedTxt, loading: false });
-      } else {
-        console.error("Failed to fetch text:", response.statusText);
-        this.setState({ loading: false });
-      }
-    } catch (error) {
-      console.error("Error fetching text:", error);
-      this.setState({ loading: false });
+      const bookTXT = await bookService.getBookTxt(id);
+      console.log(bookTXT);
+      this.setState({ loading: false, text: bookTXT });
+    } catch (err) {
+      console.log(err);
+      //TODO
+      this.setState({ loading: false, text: "BookText[errHTML] 36" });
     }
   };
-  processHtml(html: string): string {
-    // Create a new DOMParser instance
-    const parser = new DOMParser();
-    // Parse the HTML string into a document
-    const doc = parser.parseFromString(html, "text/html");
-
-    // Select all img elements and update their src attributes
-    const images = doc.querySelectorAll("img");
-    images.forEach((img) => {
-      const src = img.getAttribute("src");
-      // Check if the src is a relative URL
-      if (src && !src.startsWith("http")) {
-        // Modify the src to include the full URL
-        img.setAttribute(
-          "src",
-          `https://www.gutenberg.org/cache/epub/${this.props.match.params.id}/${src}`
-        );
-      }
-    });
-
-    // Return the modified HTML as a string
-    return doc.body.innerHTML;
-  }
 
   render() {
     const { text, loading } = this.state;
---
+++ Frontend/src/services/bookService.ts
@@ -0,0 +1,60 @@
+import { Book } from "../assets/models/favoriteBooks.models";
+import { httpService } from "./http.service";
+
+export const bookService = {
+  getBookTxt,
+  getBookById,
+};
+
+async function getBookTxt(id: string) {
+  try {
+    const response = await fetch(`http://127.0.0.1:2027/api/books/${id}/txt`);
+    if (response.ok) {
+      const textData = await response.text();
+      const processedTxt = processHtml(textData);
+      return processedTxt;
+    } else {
+      console.error("Failed to fetch text:", response.statusText);
+      return response.statusText;
+    }
+  } catch (error) {
+    console.error("Error fetching text:", error);
+    throw error;
+  }
+
+  function processHtml(html: string): string {
+    // Create a new DOMParser instance
+    const parser = new DOMParser();
+    // Parse the HTML string into a document
+    const doc = parser.parseFromString(html, "text/html");
+
+    // Select all img elements and update their src attributes
+    const images = doc.querySelectorAll("img");
+    if (images) {
+      images.forEach((img) => {
+        const src = img.getAttribute("src");
+        // Check if the src is a relative URL
+        if (src && !src.startsWith("http")) {
+          // Modify the src to include the full URL
+          img.setAttribute(
+            "src",
+            `https://www.gutenberg.org/cache/epub/${id}/${src}`
+          );
+        }
+      });
+    }
+
+    // Return the modified HTML as a string
+    return doc.body.innerHTML;
+  }
+}
+
+async function getBookById(id: string) {
+  try {
+    const res: Response = await httpService.get(`books/${id}`);
+    return (await res.json()) as Book;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
