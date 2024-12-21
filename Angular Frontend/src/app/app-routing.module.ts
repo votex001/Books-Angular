d21abf6ff0d62dbe846b03c504e06bdc0429ee50
votex001 <jeneea45@gmail.com>
2024-12-21 21:52:18 +0200

    new routes

--- Angular Frontend/src/app/app-routing.module.ts
+++ Angular Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { LoginPageComponent } from './pages/auth/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: '', pathMatch: 'full', component: SearchPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
