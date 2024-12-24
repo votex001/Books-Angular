883614890f48ea7982ea38edb3384617c066400f
votex001 <jeneea45@gmail.com>
2024-12-24 20:32:59 +0200

    Update search-page.component.ts

--- Angular Frontend/src/app/pages/search-page/search-page.component.ts
+++ Angular Frontend/src/app/pages/search-page/search-page.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BooksService } from '../../services/books.service';
+import { Book } from '../../models/book/book.model';
 
 @Component({
   selector: 'search-page',
   standalone: false,
-
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss',
 })
-export class SearchPageComponent {
-  books: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9,1,2312];
+export class SearchPageComponent implements OnInit {
+  public books: Book[] = [];
+  private bookService = inject(BooksService);
+
+  ngOnInit(): void {
+    this.bookService.books.subscribe({
+      next: (res) => {
+        this.books = res.results;
+      },
+    });
+  }
 }
