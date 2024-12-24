ceac0db892cc3b1ecaec684dfaeeaa211892e6bd
votex001 <jeneea45@gmail.com>
2024-12-24 22:36:10 +0200

    To be continued

---
+++ Angular Frontend/src/app/services/books/books.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class BooksService {
+// TODO нужен пайп с фильтром который может получать либо один запрос либо два и соеденчть их в зависимости от page[]
+}
