a8c60b8f6fce17767a60a2a27272edba2ecb5956
votex001 <jeneea45@gmail.com>
2024-12-28 19:21:12 +0200

    Update app-routing.module.ts

--- Angular Frontend/src/app/app-routing.module.ts
+++ Angular Frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginPageComponent } from './pages/auth/login-page/login-page.component
 import { BookDetailsComponent } from './cmps/book-details/book-details.component';
 import { bookResolver } from './resolvers/book.resolver';
 import { BookTxtComponent } from './cmps/book-txt/book-txt.component';
+import { bookTxtResolver } from './resolvers/book-txt.resolver';
 
 const routes: Routes = [
   {
@@ -13,7 +14,11 @@ const routes: Routes = [
     component: BookDetailsComponent,
     resolve: { book: bookResolver },
   },
-  { path: 'book/:id/txt', component: BookTxtComponent },
+  {
+    path: 'book/:id/txt',
+    component: BookTxtComponent,
+    resolve: { book: bookTxtResolver },
+  },
   { path: 'login', component: LoginPageComponent },
   { path: 'profile', component: ProfilePageComponent },
   {
