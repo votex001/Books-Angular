740a6475dda9488713382116b4b446a27c089ddf
votex001 <jeneea45@gmail.com>
2024-12-28 19:20:43 +0200

    new resolver

---
+++ Angular Frontend/src/app/resolvers/book-txt.resolver.ts
@@ -0,0 +1,9 @@
+import { inject } from '@angular/core';
+import { ResolveFn } from '@angular/router';
+import { BooksService } from '../services/books/books.service';
+
+export const bookTxtResolver: ResolveFn<string> = (route, state) => {
+  const id = route.params['id'];
+
+  return inject(BooksService).getBookTxt(id).pipe()
+};
