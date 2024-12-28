4216ef9d7ed341dc02e7fd63720384b63107e4fb
votex001 <jeneea45@gmail.com>
2024-12-28 17:54:12 +0200

    book details component

---
+++ Angular Frontend/src/app/cmps/book-details/book-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { Book } from '../../models/book/book.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+
+@Component({
+  selector: 'book-details',
+  standalone: false,
+
+  templateUrl: './book-details.component.html',
+  styleUrl: './book-details.component.scss',
+})
+export class BookDetailsComponent {
+  book: Book | null = null;
+  private route = inject(ActivatedRoute);
+  public langName = new Intl.DisplayNames(['en'], { type: 'language' });
+  book_ = toSignal<Book>(this.route.data.pipe(map((data) => data['book'])));
+  onAddToFavorites = async () => {
+    console.log("clicked!")
+  };
+}
