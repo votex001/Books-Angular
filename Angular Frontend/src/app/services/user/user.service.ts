539f933ae02b4e065bca7780a37a75fb5bf1ec5a
votex001 <jeneea45@gmail.com>
2025-01-02 23:29:58 +0200

    starting with user service

--- Angular Frontend/src/app/services/user/user.service.ts
+++ Angular Frontend/src/app/services/user/user.service.ts
@@ -1,14 +1,63 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, catchError, of, switchMap } from 'rxjs';
 import { User } from '../../models/user/user.model';
+import { environment } from '../../../env/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient) {}
+  private url = environment.apiUrl;
 
   private _user$ = new BehaviorSubject<User | null>(null);
-  public user$ = this._user$.asObservable()
+  public user$ = this._user$.asObservable();
+
+  private _credentials$ = new BehaviorSubject<{
+    email: string;
+    password: string | number;
+  }>({ email: '', password: '' });
+  public credentials$ = this._credentials$.asObservable();
+
+  public login() {
+    return this.credentials$.pipe(
+      switchMap(({ email, password }) => {
+        if (email && password) {
+          return this.http
+            .post<User>(
+              `${this.url}/auth/login`,
+              { email, password },
+              { withCredentials: true }
+            )
+            .pipe(
+              catchError((error) => {
+                console.error('Login error:', error);
+                return of(null); // Or handle accordingly
+              })
+            );
+        } else {
+          return this.http
+            .post<User>(
+              `${this.url}/user/verifyToken`,
+              {},
+              { withCredentials: true }
+            )
+            .pipe(
+              catchError((error) => {
+                console.error('Token verification error:', error);
+                return of(null); // Or handle accordingly
+              })
+            );
+        }
+      })
+    );
+  }
+
+  public setCredentials(credentials: {
+    email: string;
+    password: string | number;
+  }) {
+    this._credentials$.next(credentials);
+  }
 }
