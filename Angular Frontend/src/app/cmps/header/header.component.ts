9f22de2d27b2488b95d271b24d54e80251d1ab94
votex001 <jeneea45@gmail.com>
2025-01-26 08:33:30 +0200

    reactive login logout

--- Angular Frontend/src/app/cmps/header/header.component.ts
+++ Angular Frontend/src/app/cmps/header/header.component.ts
@@ -1,31 +1,60 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../models/user/user.model';
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'], // Fixed the property name
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   // Define links as a class property
-  links = [
-    { title: 'Profile', to: '/profile' },
-    { title: 'Login', to: '/login' },
-    {
-      title: 'Sign out',
-      to: '',
-      onClick: async (e: Event) => {
-        e.preventDefault();
-        // Replace `userService.logout()` with actual implementation
-        console.log('Logged out');
-      },
-    },
-  ];
-  constructor(private matIconRegistry: MatIconRegistry,private domSanitizer:DomSanitizer) {
+  public user: User | null = null;
+  public links: Array<{
+    title: string;
+    to?: string;
+    hidden?: boolean;
+    onClick?: (e: Event) => void;
+  }> = [];
+
+  public hidden: boolean = false;
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private userService: UserService
+  ) {
     this.matIconRegistry.addSvgIcon(
-      "logo",
-      this.domSanitizer.bypassSecurityTrustResourceUrl("assets/logo.svg")
-    )
+      'logo',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/logo.svg')
+    );
+    this.userService.login().subscribe({
+      next: (user) => {
+        this.user = user;
+        this.updateLinks();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+    this.updateLinks();
+  }
+  private updateLinks() {
+    this.links = [
+      { title: 'Profile', to: '/profile', hidden: !this.user },
+      { title: 'Login', to: '/login', hidden: !!this.user },
+      {
+        title: 'Sign out',
+        hidden: !!this.user,
+        onClick: async (e: Event) => {
+          e.preventDefault();
+          await this.userService.logout();
+          this.user = null;
+          this.updateLinks();
+        },
+      },
+    ];
   }
 }
