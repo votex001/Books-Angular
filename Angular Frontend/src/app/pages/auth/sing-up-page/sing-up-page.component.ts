b9f0984b9bb3a8d36489bce0b4b79516528a5642
votex001 <jeneea45@gmail.com>
2025-01-26 13:33:14 +0200

    sign up page now works

--- Angular Frontend/src/app/pages/auth/sing-up-page/sing-up-page.component.ts
+++ Angular Frontend/src/app/pages/auth/sing-up-page/sing-up-page.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { UserService } from '../../../services/user/user.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sing-in-page',
@@ -7,4 +16,63 @@ import { Component } from '@angular/core';
   templateUrl: './sing-up-page.component.html',
   styleUrl: './sing-up-page.component.scss',
 })
-export class SingUpPageComponent {}
+export class SingUpPageComponent {
+  private subscription: Subscription | null = null;
+
+  isErr: boolean = false;
+  signUpForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      fullName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPass: new FormControl('', [Validators.required]),
+    },
+    { validators: this.passwordsMatchValidator }
+  );
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  private passwordsMatchValidator(
+    formGroup: AbstractControl
+  ): { [key: string]: boolean } | null {
+    const password = formGroup.get('password')?.value;
+    const confirmPass = formGroup.get('confirmPass')?.value;
+
+    if (password !== confirmPass) {
+      return { passwordsMismatch: true }; // Return an error if they don't match
+    }
+
+    return null; // No error
+  }
+  public onSubmit = () => {
+    this.resetErrors();
+    if (!this.signUpForm.valid) {
+      this.isErr = true;
+      return;
+    }
+    const { email, password, fullName } = this.signUpForm.value;
+    if (email && password && fullName) {
+      this.subscription = this.userService
+        .signUp({ email, password, fullName })
+        .subscribe({
+          next: (ans: any) => {
+            if (ans.status === 201) {
+              this.router.navigate([`/confirm/${email}`]);
+            }
+          },
+          error: (err) => {
+            console.log(err);
+          },
+          complete: () => {
+            if (this.subscription) {
+              this.subscription.unsubscribe();
+            }
+          },
+        });
+    }
+  };
+
+  public resetErrors = () => {
+    this.isErr = false;
+  };
+}
