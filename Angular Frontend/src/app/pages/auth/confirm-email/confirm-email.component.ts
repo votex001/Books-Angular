309027865a68de4e307031dab9ac1a8982dc14ba
votex001 <jeneea45@gmail.com>
2025-01-26 13:32:20 +0200

    work on confirm email

--- Angular Frontend/src/app/pages/auth/confirm-email/confirm-email.component.ts
+++ Angular Frontend/src/app/pages/auth/confirm-email/confirm-email.component.ts
@@ -3,10 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'confirm-email',
   standalone: false,
-  
-  templateUrl: './confirm-email.component.html',
-  styleUrl: './confirm-email.component.scss'
-})
-export class ConfirmEmailComponent {
 
-}
+  templateUrl: './confirm-email.component.html',
+  styleUrl: './confirm-email.component.scss',
+})
+export class ConfirmEmailComponent {}
